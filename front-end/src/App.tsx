bfaff79fe6d25f2048e811fd1daaf3c88d78e95a
MarcosKern <vkg.marcos@gmail.com>
2023-02-21 14:21:50 -0300

    clean files

---
+++ front-end/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      {
+        // content
+      }
+    </div>
+  )
+}
+
+export default App
