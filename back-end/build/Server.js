0a67cfe3e14ef1acb1a0147cacb81a60308f1738
MarcosKern <vkg.marcos@gmail.com>
2023-02-24 18:22:09 -0300

    chenged to create react app

--- back-end/build/Server.js
+++ back-end/build/Server.js
@@ -3,7 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
 const App_1 = __importDefault(require("./App"));
-const PORT = 3306;
+const PORT = process.env.PORT || '3306';
 const app = new App_1.default();
 app.start(PORT);
